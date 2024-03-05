dbf4cf15a8f6486957b7f2c95ba385b0bf55b70a
KiraManolo.LENOVO-NIX <21977499+JunkManuel@users.noreply.github.com>
2024-03-05 13:37:03 +0100

    testing hyprland shit

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -7,7 +7,7 @@ in {
     options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
     config = mkIf cfg.enable {
 	home.packages = with pkgs; [
-	    rofi-wayland hyprpaper wlsunset wl-clipboard hyprland
+	    rofi-wayland hyprpaper wlsunset wl-clipboard # hyprland
 	];
     home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
     home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;

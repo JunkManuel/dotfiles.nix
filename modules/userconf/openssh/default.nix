bca81ddc2f35bfba07e0e50cfc87639819a8107f
KiraManolo.LENOVO-NIX <21977499+JunkManuel@users.noreply.github.com>
2024-06-06 20:04:40 +0200

    ssh shit

--- modules/userconf/openssh/default.nix
+++ modules/userconf/openssh/default.nix
@@ -18,6 +18,10 @@ in {
                     Hostname            kiramanolo.ddns.net
                     IdentityFile        ~/.ssh/id_ed25519_oracle
                     IdentitiesOnly      yes
+                Host 144.24.201.50
+                    Hostname            kiramanolo.ddns.net
+                    IdentityFile        ~/.ssh/id_ed25519_oracle
+                    IdentitiesOnly      yes
                 '';
         };
     };
